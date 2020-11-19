df46ce067b89d98bc09af6c1f670e72fa999f388
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-11-19 23:26:51 +0100

    Add files via upload

---
+++ spring boot spring security jwt authentication/src/main/java/com/protech/springjwt/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.protech.springjwt.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.protech.springjwt.models.ERole;
+import com.protech.springjwt.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByName(ERole name);
+}
